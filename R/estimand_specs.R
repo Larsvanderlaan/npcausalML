a8d85ca134e3648e712ef5d5afb56f7baf916104
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-04-27 16:21:05 -0700

    update

--- R/estimand_specs.R
+++ R/estimand_specs.R
@@ -1,14 +1,18 @@
 # CATE
+#' @export
 outcome_function_plugin_CATE <- function(A, Y, EY1W, EY0W, pA1W) {
   EY1W - EY0W
 }
+#' @export
 weight_function_plugin_CATE <- function(A, Y, EY1W, EY0W, pA1W) {
   rep(1, length(A))
 }
+#' @export
 outcome_function_IPW_CATE <- function(A, Y, EY1W, EY0W, pA1W) {
   pA0W <- 1- pA1W
   Y * (A/pA1W - (1-A)/(pA0W))
 }
+#' @export
 weight_function_IPW_CATE <- function(A, Y, EY1W, EY0W, pA1W) {
   #pA <- ifelse(A==1, pA1W, 1 - pA1W)
   #1 / pA
@@ -16,21 +20,26 @@ weight_function_IPW_CATE <- function(A, Y, EY1W, EY0W, pA1W) {
 }
 
 
-
+#' @export
 design_function_sieve_plugin_CATE <- function(X,A , Y, EY1W , EY0W , pA1W ) {
   cbind(A*X, (1-A)*X)
 }
+#' @export
 design_function_sieve_IPW_CATE <- function(X,A , Y, EY1W , EY0W , pA1W ){
   pA0W <- 1-pA1W
   cbind(EY1W/pA1W * X, X* EY0W/pA0W)
 }
+#' @export
 weight_function_sieve_plugin_CATE <- function(A , Y, EY1W , EY0W , pA1W ){
   1/ifelse(A==1,pA1W, 1- pA1W)
 }
+#' @export
 weight_function_sieve_IPW_CATE <- function(A , Y, EY1W , EY0W , pA1W ){
   return(rep(1,length(A)))
 }
 
+
+#' @export
 efficient_loss_function_CATE <- function(theta, A , Y, EY1W , EY0W , pA1W){
   pA <-  1/ifelse(A==1,pA1W, 1- pA1W)
   EY <- ifelse(A==1, EY1W, EY0W)
@@ -41,7 +50,7 @@ efficient_loss_function_CATE <- function(theta, A , Y, EY1W , EY0W , pA1W){
 
 
 ## RR
-
+#' @export
 outcome_function_plugin_LRR <- function(A, Y, EY1W, EY0W, pA1W) {
   #print("outcome plugin")
 
@@ -49,35 +58,43 @@ outcome_function_plugin_LRR <- function(A, Y, EY1W, EY0W, pA1W) {
   #print(quantile((EY1W  / (EY1W + EY0W))))
   EY1W  / (EY1W + EY0W)
 }
+#' @export
 weight_function_plugin_LRR <- function(A, Y, EY1W, EY0W, pA1W) {
   (EY1W + EY0W)
   #print("weight")
   #print(quantile((  (EY1W + EY0W))))
   (EY1W + EY0W)
 }
+#' @export
 outcome_function_IPW_LRR <- function(A, Y, EY1W, EY0W, pA1W) {
   A
 }
+#' @export
 weight_function_IPW_LRR <- function(A, Y, EY1W, EY0W, pA1W) {
   pA <- ifelse(A==1, pA1W, 1 - pA1W)
   Y / pA
 }
 
+#' @export
 design_function_sieve_plugin_LRR <- function(X,A , Y, EY1W , EY0W , pA1W ) {
   cbind(A* X, X*(1-A))
 }
+#' @export
 design_function_sieve_IPW_LRR <- function(X,A , Y, EY1W , EY0W , pA1W ){
   pA0 <- 1-pA1W
 
   cbind(EY1W/pA1W * X, EY0W/pA0 * X)
 }
+#' @export
 weight_function_sieve_plugin_LRR <- function(A , Y, EY1W , EY0W , pA1W ){
   1/ifelse(A==1,pA1W, 1- pA1W)
 }
+#' @export
 weight_function_sieve_IPW_LRR <- function(A , Y, EY1W , EY0W , pA1W ){
   return(rep(1,length(A)))
 }
 
+#' @export
 efficient_loss_function_LRR <- function(theta, A , Y, EY1W , EY0W , pA1W){
   LRR <- theta
   EY <- ifelse(A==1, EY1W, EY0W)
