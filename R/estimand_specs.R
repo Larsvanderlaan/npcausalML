13f2954220ed88cc98201fb96d7e3ca5f17227c1
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-07 13:49:31 -0700

    :hi

--- R/estimand_specs.R
+++ R/estimand_specs.R
@@ -105,6 +105,6 @@ efficient_loss_function_LRR <- function(theta, A , Y, EY1W , EY0W , pA1W,...){
 
 
 
-
+#' @export
 EP_learner_spec_CATE <- list(efficient_loss_function = efficient_loss_function_CATE, outcome_type = "continuous", sieve_design_transform = design_function_sieve_plugin_CATE, sieve_weight_transform  = weight_function_sieve_plugin_CATE, EP_family = gaussian(), EP_outcome_transform = outcome_function_plugin_CATE, EP_weight_transform = weight_function_plugin_CATE)
 
