2c6163c6a2ddd4fb2d9922b75081f6ebdb151614
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-07 15:34:28 -0700

    all

--- FinalSimulationCode/simsCATE_gam_highdim.R
+++ FinalSimulationCode/simsCATE_gam_highdim.R
@@ -2,7 +2,7 @@ library(SuperLearner)
 library(npcausalML)
 library(future)
 source("./FinalSimulationCode/simCATEHighDim.R")
-plan(cluster, workers = 3)
+#plan(cluster, workers = 3)
 SL.gam1 <- function(Y, X, newX, family, obsWeights, cts.num = 4,...) {
   deg.gam <- 1
   SuperLearner::SL.gam(Y, X, newX, family, obsWeights, deg.gam, cts.num,... )
