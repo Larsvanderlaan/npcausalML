ef3ae01e439251bb7842f81c0bb0cd6dc056f159
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-14 13:53:10 -0700

    hi

--- BatchScripts/test.R
+++ BatchScripts/test.R
@@ -1,2 +1,3 @@
-devtools::install_github("tlverse/sl3")
+devtools::install_cran("hardhat")
+devtools::install_github("tlverse/sl3", ref = "devel")
 devtools::install_github("Larsvanderlaan/npcausalML")
